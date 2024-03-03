0d49684eb2d349077662a7bbdbb622cd08d8f8cd
WLira <liracalero1996@gmail.com>
2024-03-02 18:33:42 -0600

    Tarea 2 agregada, tareas con operadores

--- scripts/scripts1.js
+++ scripts/scripts1.js
@@ -15,3 +15,36 @@ function tarea2(){
     console.log("planetName= "+planetName);
     console.log("currentUserName= "+currentUserName);
 }
+
+function tarea3(){
+    console.clear();
+    console.log("" + 1 + 0);
+    console.log("" - 1 + 0);
+    console.log(true + false);
+    console.log(6 / "3");
+    console.log("2" * "3");
+    console.log(4 + 5 + "px");	
+    console.log("$" + 4 + 5);
+    console.log("4" - 2);
+    console.log("4px" - 2);
+    console.log("  -9  " + 5);		
+    console.log("  -9  " - 5);	
+    console.log(null + 1);
+    console.log(undefined + 1);	
+    console.log(" \t \n" - 2);
+}
+
+function tarea4(){
+    let a = prompt("¿Primer número?", 1);
+    let b = prompt("¿Segundo número?", 2);
+    a = parseInt(a);
+    b = parseInt(b);
+    alert(a + b); // 12
+    /*Otra solucion es agregar un + 
+        Ejemplo
+        let a = +prompt("¿Primer número?", 1);
+        let b = +prompt("¿Segundo número?", 2);
+        
+        Una tercer solucion es:
+        alert(+a + +b);*/
+}
