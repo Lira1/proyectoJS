596d1d31d2c0b9ea0b87d184624e06ca33da6ae5
WLira <liracalero1996@gmail.com>
2024-03-05 19:56:38 -0600

    agregando tarea 5, manipulacion del DOM

--- scripts/scripts2.js
+++ scripts/scripts2.js
@@ -1,5 +1,4 @@
 // your JavaScript file
-console.log("Esto se ejecuta?")
 const container = document.querySelector('#container');
 
 const content = document.createElement('div');
@@ -7,3 +6,34 @@ content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
 
 container.appendChild(content);
+
+const tagP = document.createElement('p');
+tagP.classList.add('parrafo');
+tagP.textContent = '¡Oye, soy rojo!';
+tagP.style.color = 'red';
+
+container.appendChild(tagP);
+
+const encabezado3 = document.createElement('h3');
+encabezado3.classList.add('encabezado');
+encabezado3.textContent = '¡Soy un h3 azul!';
+encabezado3.style.color = 'blue';
+
+container.appendChild(encabezado3);
+
+const divHijo = document.createElement('div');
+divHijo.classList.add('div-hijo');
+divHijo.style.cssText= 'border: 2px solid black; background-color: pink';
+divHijo.textContent = "hla";
+container.appendChild(divHijo);
+
+/*
+Agregue los siguientes elementos al contenedor usando SÓLO JavaScript y los métodos DOM que se muestran arriba.
+
+a <p>con texto rojo que dice "¡Oye, soy rojo!"
+un <h3>con texto azul que dice "¡Soy un h3 azul!"
+a <div>con un borde negro y color de fondo rosa con los siguientes elementos en su interior:
+otro <h1>que dice "estoy en un div"
+un <p>que dice “¡YO TAMBIÉN!”
+Sugerencia para este: después de crear <div>con createElement, añádale <h1>y <p>antes de agregarlo al contenedor.
+ */
