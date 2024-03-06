34eaf574f08ce5721a0d42537878504b461020cf
WLira <liracalero1996@gmail.com>
2024-03-05 19:31:27 -0600

    agregando tarea 5, manipulacion del DOM

---
+++ scripts/scripts2.js
@@ -0,0 +1,12 @@
+// const container = document.querySelector('#container');
+// const content = document.createElement('div');
+// content.classList.add('content');
+// content.textContent = 'Este es un contenido';
+// container.appendChild(content);
+const container = document.querySelector('#container');
+
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'This is the glorious text-content!';
+
+container.appendChild(content);
